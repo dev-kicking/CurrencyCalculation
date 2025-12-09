524f0accff7a286bb0656c5c97c0a07edd430f51
jacob <aoqnwnd@naver.com>
2025-12-10 00:36:01 +0900

    [chore] 미사용 코드 제거

--- feature/currency/src/main/java/dev/kick/currencycalculation/feature/currency/ui/component/CountryScrollSelector.kt
+++ feature/currency/src/main/java/dev/kick/currencycalculation/feature/currency/ui/component/CountryScrollSelector.kt
@@ -8,7 +8,6 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.rememberLazyListState
-import androidx.compose.foundation.pager.VerticalPager
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
--- feature/currency/src/main/java/dev/kick/currencycalculation/feature/currency/ui/screen/CurrencyScreen.kt
+++ feature/currency/src/main/java/dev/kick/currencycalculation/feature/currency/ui/screen/CurrencyScreen.kt
@@ -9,7 +9,6 @@ import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
